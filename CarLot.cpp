24bda7d281ba43a303dd2e220978add3f468ed21
Victor Li <vli30@my.bcit.ca>
2021-12-15 23:47:41 -0800

    Vehicle.h CarLot.cpp Practice

--- CarLot.cpp
+++ CarLot.cpp
@@ -1,19 +1,67 @@
 #include "Vehicle.h"
 #include "iostream"
+#include "vector"
 using namespace std;
 
-class CarLot{
+class CarLot {
 private:
-
+    vector<Vehicle> cars;
 public:
-
-
+    CarLot();
+    void createAndAddCar(string vin, string name, Colour colour, int price, int odometer);
+    void addCar(Vehicle car);
+    void removeCar(Vehicle car);
+    void generateStreamAndPerformServices();
+    void sortByPrice();
+    string toString();
 };
+
+CarLot::CarLot() {
+
+}
+
+void CarLot::createAndAddCar(string vin, string name, Colour colour, int price, int odometer) {
+
+}
+
+void CarLot::addCar(Vehicle car) {
+    cars.push_back(car);
+}
+
+void CarLot::removeCar(Vehicle car) {
+    vector<Vehicle>::iterator iterator;
+    iterator = cars.begin();
+    while(iterator != cars.end()) {
+        if (*iterator == car) {
+            cars.erase(iterator);
+        }
+        iterator++;
+    }
+
+}
+
+void CarLot::generateStreamAndPerformServices() {
+
+}
+
+void CarLot::sortByPrice() {
+
+}
+
+string toString() {
+}
 
 int main() {
     Vehicle toyota = Vehicle("234ads", "Toyota", JET_BLACK, 15'000, 250'000);
     Vehicle honda =  Vehicle("123abc", "Honda", CHERRY_RED,
                      20'000, 10'000);
-    cout << toyota.getOdometer();
+    CarLot myCarLot;
+    myCarLot.addCar(toyota);
+    myCarLot.addCar(honda);
+    myCarLot.removeCar(toyota);
+
+    // cout << toyota.getOdometer();
+    cout << toyota;
+    // cout << myCarLot;
     return 0;
 }
--- Vehicle.h
+++ Vehicle.h
@@ -19,7 +19,35 @@ private:
 
 public:
     Vehicle(string vin, string name, Colour colour, int price, int odometer);
-    int getOdometer();
+
+    const string &getVin() const;
+
+    const string &getName() const;
+
+    int getYear() const;
+
+    Colour getColour() const;
+
+    double getPrice() const;
+
+    bool isElectric1() const;
+
+    int getNumberOfDoors() const;
+
+    int getNumberOfWheelDrive() const;
+
+    int getOdometer() const;
+
+    void setPrice(double price);
+
+    void setOdometer(int newOdometer);
+
+    bool operator==(const Vehicle &rhs) const;
+
+    bool operator!=(const Vehicle &rhs) const;
+
+    friend ostream &operator<<(ostream &os, const Vehicle &vehicle);
+
 };
 
 Vehicle::Vehicle(string vin, string name, Colour colour, int price, int odometer) {
@@ -30,6 +58,70 @@ Vehicle::Vehicle(string vin, string name, Colour colour, int price, int odometer
     this->odometer = odometer;
 }
 
-int Vehicle::getOdometer() {
+const string &Vehicle::getVin() const {
+    return vin;
+}
+
+const string &Vehicle::getName() const {
+    return name;
+}
+
+int Vehicle::getYear() const {
+    return year;
+}
+
+Colour Vehicle::getColour() const {
+    return colour;
+}
+
+double Vehicle::getPrice() const {
+    return price;
+}
+
+bool Vehicle::isElectric1() const {
+    return isElectric;
+}
+
+int Vehicle::getNumberOfDoors() const {
+    return numberOfDoors;
+}
+
+int Vehicle::getNumberOfWheelDrive() const {
+    return numberOfWheelDrive;
+}
+
+int Vehicle::getOdometer() const {
     return this->odometer;
+}
+
+void Vehicle::setPrice(double newPrice) {
+    Vehicle::price = newPrice;
+}
+
+void Vehicle::setOdometer(int newOdometer) {
+    Vehicle::odometer = newOdometer;
+}
+
+bool Vehicle::operator==(const Vehicle &rhs) const {
+    return vin == rhs.vin &&
+           name == rhs.name &&
+           year == rhs.year &&
+           colour == rhs.colour &&
+           price == rhs.price &&
+           isElectric == rhs.isElectric &&
+           numberOfDoors == rhs.numberOfDoors &&
+           numberOfWheelDrive == rhs.numberOfWheelDrive &&
+           odometer == rhs.odometer;
+}
+
+bool Vehicle::operator!=(const Vehicle &rhs) const {
+    return !(rhs == *this);
+}
+
+ostream &operator<<(ostream &os, const Vehicle &vehicle) {
+    os << "vin: " << vehicle.vin << " name: " << vehicle.name << " year: " << vehicle.year << " colour: "
+       << vehicle.colour << " price: " << vehicle.price << " isElectric: " << vehicle.isElectric << " numberOfDoors: "
+       << vehicle.numberOfDoors << " numberOfWheelDrive: " << vehicle.numberOfWheelDrive << " odometer: "
+       << vehicle.odometer;
+    return os;
 }
